87d390c8360496c3897ba3d9495fe6f1ca452498
John <45529374+Johnhandsome@users.noreply.github.com>
2025-05-29 23:40:40 +0700

    Add files via upload

---
+++ SortArray/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+void swap(ref int a,ref int b)
+{
+    int temp = a;
+    a=b;
+    b=temp;    
+}
+void sort_arr(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[i]>arr[j])
+            {
+                swap(ref arr[i],ref arr[j]);
+            }    
+        }
+    }
+}
+void create_array(int[]arr)
+{
+    Random rd=new Random(); 
+    for(int i=0; i<arr.Length; i++)
+    {
+        arr[i] = rd.Next(100);
+    }    
+}
+void print_array(int  []arr)
+{
+    foreach(int x in arr)
+    {
+        Console.Write($"{x}\t");
+    }    
+}
+int[]arr= new int[10];
+create_array(arr);
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Mảng trước khi sắp xếp");
+print_array(arr);
+Console.WriteLine("Mảng sau khi sắp xếp");
+sort_arr(arr);
+print_array(arr);
